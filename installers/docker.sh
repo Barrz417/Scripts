097478aecf3ca3d1f093469690a12529c04b6e0d
Erez Binamin <ezbin@pm.me>
2025-10-23 04:51:33 -0400

    Installers: docker.sh updated

--- installers/docker.sh
+++ installers/docker.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
+# Source: https://docs.docker.com/engine/install/ubuntu/#install-using-the-repository
 export PS4='\033[0;33m$0:$LINENO [$?]+ \033[0m '
 set -x
+
+ununstall_docker() {
+	# 1. Uninstall the Docker Engine, CLI, containerd, and Docker Compose packages:
+	sudo apt-get purge docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-ce-rootless-extras
+	# 2. Images, containers, volumes, or custom configuration files on your host aren't automatically removed. To delete all images, containers, and volumes:
+	sudo rm -rf /var/lib/docker
+	sudo rm -rf /var/lib/containerd
+	# 3. Remove source list and keyrings
+	sudo rm /etc/apt/sources.list.d/docker.list
+	sudo rm /etc/apt/keyrings/docker.asc
+	# 4. 
+	echo "You have to delete any edited configuration files manually."
+}
 
 install_docker() {
 	# Install Docker
@@ -8,37 +22,49 @@ install_docker() {
 	then
 		echo [COMPLETE] docker already installed!
 	else
-		pushd "${PWD}"
-		cd /tmp
+		#1. Set up Docker's apt repository.
+		# Add Docker's official GPG key:
+		sudo apt-get update
+		sudo apt-get install ca-certificates curl
+		sudo install -m 0755 -d /etc/apt/keyrings
+		sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+		sudo chmod a+r /etc/apt/keyrings/docker.asc
+		# Add the repository to Apt sources:
+		echo \
+		  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+		  $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
+		  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+		sudo apt-get update
 
-		# 1. download the script
-		curl -fsSL https://get.docker.com -o install-docker.sh
+		# 2. Install the Docker packages.
+		sudo apt-get install --yes \
+			docker-ce \
+			docker-ce-cli \
+			containerd.io \
+			docker-buildx-plugin \
+			docker-compose-plugin
 
-		# 2. verify the script's content
-		#cat install-docker.sh
-
-		# 3. run the script with --dry-run to verify the steps it executes
-		sh install-docker.sh --dry-run
-
-		# 4. run the script either as root, or using sudo to perform the installation.
-		sudo sh install-docker.sh
-
-		# linux-postinstall: https://docs.docker.com/engine/install/linux-postinstall/
-		# Manage Docker as a non-root user
-		sudo groupadd docker
-		sudo usermod -aG docker $USER
-		newgrp docker
-		docker run hello-world
-
-		# Configure Docker to start on boot with systemd
-		sudo systemctl enable docker.service; sleep 2
-		sudo systemctl enable containerd.service; sleep 2
-		sudo systemctl disable docker.service; sleep 2
-		sudo systemctl disable containerd.service
-
-		echo [COMPLETE] docker installed!
-		popd
+		#3. Verify that the installation is successful
+		sudo systemctl status docker || return 1
 	fi
+}
+
+postinstall_docker() {
+	# linux-postinstall: https://docs.docker.com/engine/install/linux-postinstall/
+	# Manage Docker as a non-root user
+	sudo groupadd docker
+	sudo usermod -aG docker $USER
+	newgrp docker
+	docker run hello-world
+
+	# Configure Docker to start on boot with systemd
+	sudo systemctl enable docker.service; sleep 2
+	sudo systemctl enable containerd.service; sleep 2
+	sudo systemctl disable docker.service; sleep 2
+	sudo systemctl disable containerd.service
+
+	echo [COMPLETE] docker installed!
+	popd
 
 	# Docker images
 	# DOCKER_IMAGES=()
@@ -52,4 +78,11 @@ install_docker() {
 	# done
 }
 
-install_docker $@
+case "$#" in
+	"0" | "1")
+		install_docker
+		;;
+	*)
+		postinstall_docker 
+		;;
+esac
